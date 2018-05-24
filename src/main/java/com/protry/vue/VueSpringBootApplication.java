725e63b483ef8c8878ab47c6bc9282af9016d3f3
protry <bandaohongyu@gmail.com>
2018-05-24 17:22:38 +0800

    first commit

---
+++ src/main/java/com/protry/vue/VueSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.protry.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueSpringBootApplication.class, args);
+    }
+}
